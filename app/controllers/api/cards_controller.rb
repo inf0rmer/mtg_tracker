90ee8d8e77274bb7f333441a9de595199004e8d1
Bruno Abrantes <bruno@brunoabrantes.com>
2021-05-07 17:18:45 +0200

    Do not expose card names in Prometheus format

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -3,6 +3,8 @@ class Api::CardsController < ApplicationController
   def index
     @cards = Card.all
 
+    fresh_when last_modified: @cards.maximum(:updated_at), public: true
+
     respond_to do |format|
       format.json       { render :json => @cards }
       format.prometheus { render :prometheus => @cards }
